90c837be65c0c3ba30d2f98ae7158b960f0921c4
Anatolii Stehnii <tsdaemon@gmail.com>
2024-08-02 08:59:31 +0000

    Add connectors

--- craftai/entities/connector.py
+++ craftai/entities/connector.py
@@ -1,12 +1,14 @@
+from typing import Any
+
 import reflex as rx
 
-from craftai.entities.base import Listable
+from craftai.entities.listable import Listable
 
 
 class Connector(rx.Base, Listable):
     name: str
     connector_type: str
-    connector_data: dict[str, str]
+    connector_data: dict[str, Any]
 
     def check_search_string(self, value: str) -> bool:
         return value.lower() in self.name.lower() or value.lower() in self.connector_type.lower()
